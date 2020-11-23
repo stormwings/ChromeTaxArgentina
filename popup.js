459e74fccce6fcffab5cd0ca8cd75a0b1d384e4b
Mariano Villarreal <marianov994@gmail.com>
2020-11-23 20:11:27 -0300

    release v0.1

--- popup.js
+++ popup.js
@@ -1,39 +1,45 @@
 window.onload = function() {
-  const $inputName = document.getElementById('name');
-  const $itemToSearch = document.getElementById('selected_item');
+  // const $inputPrice = document.getElementById('manual_amount_input');
+  const $itemName = document.getElementsByClassName('tax__header--page_title')[0];
+  const $itemPrice = document.getElementsByClassName('tax__header--find_item')[0];
+  let $articlePriceContainer = null;
 
-  const $article_name = document.querySelector('#greetings');
-  const $tax_iva = document.querySelector('.tax_iva > .amount > .value');
-  const $tax_pais = document.querySelector('.tax_pais > .amount > .value');
-  const $tax_ret = document.querySelector('.tax_ret > .amount > .value');
-  const $tax_total = document.querySelector('.tax_total > .amount > .value');
-  const $tax_final = document.querySelector('#total_final');
-  
-  if ($inputName) {
-    $inputName.addEventListener("keyup", event => {
-      const { target: { value } } = event;
-  
-      console.log(value);
-    });
-  }
+  // show amounts
 
   setTimeout(() => {
-    const $articleName = document.querySelector('.apphub_AppName').innerHTML;
-    const $articlePriceContainer = getAllElementsWithAttribute('data-price-final')[0];
-    const articlePrice = transformToFiat($articlePriceContainer.getAttribute('data-price-final'));
+    const articlePrice = transformToFiat($articlePriceContainer);
     
-    $itemToSearch.innerHTML = `Precio: $${articlePrice}`;
-    $article_name.innerHTML = $articleName;
+    $itemPrice.innerHTML = `$${articlePrice}`;
 
+    document.querySelector('.tax_iva > .amount > .amount--value').innerHTML = `$${(articlePrice * 0.21).toFixed(2)}`;
+    document.querySelector('.tax_pais > .amount > .amount--value').innerHTML = `$${(articlePrice * 0.08).toFixed(2)}`;
+    document.querySelector('.tax_ret > .amount > .amount--value').innerHTML = `$${(articlePrice * 0.35).toFixed(2)}`;
+    document.querySelector('.tax_total > .amount > .amount--value').innerHTML = `$${(articlePrice * 0.64).toFixed(2)}`;
+    document.querySelector('#total_final').innerHTML = `Precio Final: $${articlePrice + (articlePrice * 0.64)}`;
+  }, 700)
 
-    $tax_iva.innerHTML = `$${(articlePrice * 0.21).toFixed(2)}`;
-    $tax_pais.innerHTML = `$${(articlePrice * 0.08).toFixed(2)}`;
-    $tax_ret.innerHTML = `$${(articlePrice * 0.35).toFixed(2)}`;
-    $tax_total.innerHTML = `$${(articlePrice * 0.64).toFixed(2)}`;
+  // events
+  
+  // $inputPrice.addEventListener("keyup", event => {
+  //   const { target: { value } } = event;
 
-    $tax_final.innerHTML = `Precio Final: $${articlePrice + (articlePrice * 0.64)}`;
-  }, 1000)
+  //   console.log(value);
+  // });
+
+  chrome.tabs.query({active: true}, function(tabs) {
+    chrome.tabs.executeScript(tabs[0].id, {
+      code: 'document.querySelector(".apphub_AppName").textContent'
+    }, (results) => $itemName.innerHTML = results);
+  });
+
+  chrome.tabs.query({active: true}, function(tabs) {
+    chrome.tabs.executeScript(tabs[0].id, {
+      code: 'document.querySelector(".price").textContent'
+    }, (results) => $articlePriceContainer = results ? results : null);
+  });
 }
+
+// helper utils
 
 const getAllElementsWithAttribute = (attribute) => {
   let matchingElements = [];
@@ -48,8 +54,10 @@ const getAllElementsWithAttribute = (attribute) => {
   return matchingElements;
 }
 
+const removeElements = (item) => item.replace(/\D/g, "")
+
 const transformToFiat = (number) => {
-  const numberString = number.toString();
+  const numberString = removeElements(number.toString());
   const entires = numberString.substring(0, numberString.length - 2);
   const decimals = numberString.slice(-2);
 
